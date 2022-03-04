cb71414e159a39789bcdb920e215e614aaa8218c
Khalitov62 <tu7m5rk73code.edu.az>
2022-03-05 03:51:08 +0400

    homework added

---
+++ Program2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 123;
+            while (num > 0)
+            { 
+
+            }
+        }
+    }
+}
