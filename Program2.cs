4a3f6b162b35689cec3be63159e080990fc10812
Khalitov62 <tu7m5rk73code.edu.az>
2022-03-05 13:03:25 +0400

    HomeWork added

--- Program2.cs
+++ Program2.cs
@@ -6,10 +6,13 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            int num = 123;
+            int num = Convert.ToInt32(Console.Readline());
+            int sum = 0;
             while (num > 0)
             { 
-
+               sum += num %10
+                    num /=10;
+                Console.WriteLine(sum);
             }
         }
     }
